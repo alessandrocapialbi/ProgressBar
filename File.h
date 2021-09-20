090de9d8f63b89f782422d01b6890204521bfac8
alessandrocapialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2021-09-21 00:47:20 +0200

    Adding support for unit testing using Google Test.

--- File.h
+++ File.h
@@ -18,7 +18,6 @@ public:
 
     int getSize() const {
         return fileSize;
-
     }
 
 
---
+++ test/runAllTests.cpp
@@ -0,0 +1,11 @@
+//
+// Created by alessandro on 21/09/21.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
